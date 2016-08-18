e604aff367990d8e2fb8d68ada65e05d4f8f054e
Brandon <brandon.ma@live.ca>
2016-08-18 01:44:16 -0400

    Added Charts to display data
    rough skeletal design of the charts. Placeholder data

--- scripts.js
+++ scripts.js
@@ -10,6 +10,17 @@ $(document).ready(function() {
 			getWeather();
 		}
 	});
+	
+	loadCharts();
+	
+	$(".tabs-menu a").click(function(event) {
+        event.preventDefault();
+        $(this).parent().addClass("current");
+        $(this).parent().siblings().removeClass("current");
+        var tab = $(this).attr("href");
+        $(".tab-content").not(tab).css("display", "none");
+        $(tab).fadeIn();
+    });
 });
 
 function getWeather() {
@@ -25,12 +36,16 @@ function getWeather() {
 		type: "GET",
 		async: "false",
 		success: function(data) {
+			console.log(data);
+			console.log(data.city.name);
+			
 			// Parse through the json data and extract the necessary information to display in the page dynamically
 			document.getElementById("cityName").innerHTML = data.city.name + ", " + data.city.country;
 			
 			replaceInnerHtml("weatherList", ""); // Clear the Weekly Forecast container
 			
 			for (i = 0; i < data.list.length; i++) {
+				console.log(data.list[i]);
 				// If it's today's information, update the main header section with extra data
 				if (i == 0) {
 					replaceInnerHtml ("cityTemp", Math.round(data.list[i].temp.day));
@@ -68,4 +83,50 @@ function replaceInnerHtml (elementId, info) {
 
 function capitalizeFirstLetter(string) {
 	return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+
+// Charts scripts
+
+function loadCharts() {
+	var ctx = document.getElementById("tempChart");
+	var myChart = new Chart(ctx, {
+		type: 'line',
+		data: {
+			labels: ["January", "February", "March", "April", "May", "June", "July"],
+			datasets: [
+				{
+					label: "My First dataset",
+					fill: true,
+					lineTension: 0.1,
+					backgroundColor: "rgba(75,192,192,0.3)",
+					borderColor: "rgba(75,192,192,1)",
+					borderCapStyle: 'butt',
+					borderDash: [],
+					borderDashOffset: 0.0,
+					borderJoinStyle: 'miter',
+					pointBorderColor: "rgba(75,192,192,1)",
+					pointBackgroundColor: "#fff",
+					pointBorderWidth: 1,
+					pointHoverRadius: 5,
+					pointHoverBackgroundColor: "rgba(75,192,192,1)",
+					pointHoverBorderColor: "rgba(220,220,220,1)",
+					pointHoverBorderWidth: 2,
+					pointRadius: 1,
+					pointHitRadius: 10,
+					data: [65, 59, 80, 81, 56, 55, 40],
+					spanGaps: false,
+				}
+			]
+		},
+		options: {
+			scales: {
+				yAxes: [{
+					ticks: {
+						beginAtZero:true
+					}
+				}]
+			}
+		}
+	});
 }
